07491a3ecd9261f8ba714224e8dacc8975d5b52a
False-Ai <kevindsilva39@gmail.com>
2025-06-28 10:28:02 +0530

    changed rps to be button based

--- rpscript.js
+++ rpscript.js
@@ -1,92 +1,74 @@
+let playerScore = 0;
+let compScore = 0;
+let roundCount = 0;
+const maxRounds = 5;
+
 function getCompChoice(){
-  let num = Math.random()*100;
-  if (num<33){
+  let num = Math.random() * 100;
+  if (num < 33) {
     return "rock";
-  }
-  else if (num<66){
+  } else if (num < 66) {
     return "paper";
-  }
-  else{
+  } else {
     return "scissors";
   }
 }
-function getHumanChoice(){
-  let humanChoice = "";
-  let tester = "";
-  while (humanChoice==""){
-    tester = (prompt("Enter your Move")).toLowerCase();
-    if (tester == "rock" ||tester == "paper" || tester =="scissors"){
-      humanChoice=tester;
-    }
-  }
-  return humanChoice
-}
-function displayScore(playerScore,compScore){
-  console.log(`Player:${playerScore} Matrix:${compScore}`);
-}
-function playGame(){
 
-function playRound(humanChoice,compChoice){
-  if(humanChoice == 'rock'){
-    if (compChoice == 'paper'){
-      console.log("You lose");
-      compScore+=1;
-    }
-    else if (compChoice == 'rock'){
-      console.log("Tie");
-    }
-    else{
-      console.log("You Win!!");
-      playerScore+=1;
-    }
+function displayScore(playerScore, compScore){
+  console.log(`Player: ${playerScore} Matrix: ${compScore}`);
+}
+
+function playRound(humanChoice, compChoice){
+  if (humanChoice === compChoice) {
+    console.log("Tie");
+  } else if (
+    (humanChoice === "rock" && compChoice === "scissors") ||
+    (humanChoice === "paper" && compChoice === "rock") ||
+    (humanChoice === "scissors" && compChoice === "paper")
+  ) {
+    console.log("You Win!!");
+    playerScore++;
+  } else {
+    console.log("You lose");
+    compScore++;
   }
-  else if(humanChoice == 'paper'){
-    if (compChoice == 'scissors'){
-      console.log("You lose");
-      compScore+=1;
-    }
-    else if (compChoice == 'paper'){
-      console.log("Tie");
-    }
-    else{
-      console.log("You Win!!");
-      playerScore+=1;
-    }
-  }
-  else if(humanChoice == 'scissors'){
-    if (compChoice == 'rock'){
-      console.log("You lose");
-      compScore+=1;
-    }
-    else if (compChoice == 'scissors'){
-      console.log("Tie");
-    }
-    else{
-      console.log("You Win!!");
-      playerScore+=1;
-    }
-  }
+
+  displayScore(playerScore, compScore);
   console.log(`Computer Played: ${compChoice}`);
-}
-let playerScore = 0;
-let compScore = 0;
-
-for(i=0;i<5;i++){
-compChoice=getCompChoice();
-humanChoice=getHumanChoice();
-playRound(humanChoice,compChoice);
-displayScore(playerScore,compScore);
-}
-if(playerScore>compScore){
-  console.log("You are the CHAMPION");
-}
-else if(compScore>playerScore){
-  console.log("You are the ULTIMATE LOSER");
-}
-else{
-  console.log("Y'all are EQUALLY RETARDED");
+  console.log('----------------------');
 }
 
-}
+let moves = document.querySelector('#moves');
 
-playGame();
+moves.addEventListener('click', (e) => {
+  if (roundCount >= maxRounds) {
+    console.log("Game over! No more rounds allowed.");
+    console.log("Final Score:");
+    displayScore(playerScore, compScore);
+    return;
+  }
+
+  let target = e.target;
+
+  if (target.classList.contains('rock')) {
+    handlePlayerMove('rock');
+  } else if (target.classList.contains('paper')) {
+    handlePlayerMove('paper');
+  } else if (target.classList.contains('scissors')) {
+    handlePlayerMove('scissors');
+  }
+});
+
+function handlePlayerMove(humanChoice){
+  if (roundCount < maxRounds) {
+    const compChoice = getCompChoice();
+    playRound(humanChoice, compChoice);
+    roundCount++;
+
+    if (roundCount === maxRounds) {
+      console.log("Game Over!");
+      console.log("Final Score:");
+      displayScore(playerScore, compScore);
+    }
+  }
+}
